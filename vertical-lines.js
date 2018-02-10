4d1365e29161c1b4965968a4773bf62043736b4b
mintywill <35755814+mintywill@users.noreply.github.com>
2018-02-09 17:06:19 -0800

    Create vertical-lines.js

---
+++ vertical-lines.js
@@ -0,0 +1,15 @@
+function mint(x, y) {
+  fill(255,255,255);
+for (var i = 5; i <= 400; i = i + 10) {
+  
+  var startX = i;
+  var startY = 0;
+  var endX = i;
+  var endY = 400;
+  line(startX, startY, endX, endY);
+}
+}
+
+// draw patch at upper-left
+
+mint(width-100, height-100); // draw patch at lower-right
