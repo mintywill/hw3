6024f5082c4fcc679657e21dcc84ba39a7e1faff
mintywill <35755814+mintywill@users.noreply.github.com>
2018-02-11 01:58:36 -0800

    Update patch.js

--- patch.js
+++ patch.js
@@ -1,13 +1,39 @@
 
-for (var x=0; x<=3; x++){
+function mint(x, y) {
+  fill(231, 97, 97);
+  stroke(238);
+  rect(x, y, 100, 100);
   
-noFill();
-for (var i = 10; i <= 80; i = i+10) {
-  var a = i+10*x;
-  var b = i;
-  var c = 100-2*i;
-  var d = 100-2*i;
-rect(a,b,c,d);
-}
+  // cross
+  fill(255, 247, 226);
+  noStroke();
+  rect(x+30, y+30, 40, 40);
+  rect(x+10, y+45, 80, 10);
+  rect(x+45, y+10, 10, 80);
   
+  // center
+  fill(231, 97, 97);
+  noStroke();
+  rect(x+42.5, y+42.5, 15, 15);
+
+  // others
+  fill(255, 247, 226);
+  noStroke();
+  rect(x+15, y+15, 15, 15);
+  
+  fill(255, 247, 226);
+  noStroke();
+  rect(x+15, y+70, 15, 15);
+  
+  fill(255, 247, 226);
+  noStroke();
+  rect(x+70, y+70, 15, 15);
+
+  fill(255, 247, 226);
+  noStroke();
+  rect(x+70, y+15, 15, 15);
 }
+
+
+background(255);
+mint(0,0); // draw patch at upper-left
