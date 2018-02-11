c2c888f74a679e1433e403611c2117e895734b29
mintywill <35755814+mintywill@users.noreply.github.com>
2018-02-11 02:39:22 -0800

    Create drawing-eraser.js

---
+++ drawing-eraser.js
@@ -0,0 +1,28 @@
+
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'Y') {
+    fill(255, 247, 226);
+  } else if (key == 'M') {
+    fill(143, 239, 210);
+  } else if (key == 'S') {
+    fill(239, 143, 143); 
+  } else if (key == 'C') {
+    fill(244, 140, 66); 
+	} else if (key == 'E') {
+    fill(255); noStroke();
+  } 
+}
+	
